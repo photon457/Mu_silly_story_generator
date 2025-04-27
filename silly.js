5a3d6117438686c61f5ef23ff6845a8a308e4412
photon457 <127064542+photon457@users.noreply.github.com>
2025-04-27 22:47:33 +0530

    Add files via upload

---
+++ silly.js
@@ -0,0 +1,33 @@
+const customName = document.getElementById('customName');
+const generate = document.getElementById('generate');
+const story = document.getElementById('story');
+
+function randomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+let storyText = "It was a hot day, and :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+generate.addEventListener('click', () => {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:', xItem);
+  newStory = newStory.replaceAll(':inserty:', yItem);
+  newStory = newStory.replaceAll(':insertz:', zItem);
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+});
